4bb7c3f4532e8ed6e58fc8baf5a61eef1a439184
Víctor M Gomez <34237832+Victor96Man@users.noreply.github.com>
2019-02-23 11:52:41 +0100

    Eliminar y crear carpeta uploads en cada ejecucion

--- src/main/java/com/bolsadeideas/springboot/app/SpringBootDataJpaApplication.java
+++ src/main/java/com/bolsadeideas/springboot/app/SpringBootDataJpaApplication.java
@@ -1,13 +1,27 @@
 package com.bolsadeideas.springboot.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringBootDataJpaApplication {
+import com.bolsadeideas.springboot.app.models.service.UploadsServiceImpl;
 
+@SpringBootApplication
+public class SpringBootDataJpaApplication implements CommandLineRunner{
+
+	@Autowired
+	private UploadsServiceImpl uploadsServiceImpl;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDataJpaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		uploadsServiceImpl.deleteAll();
+		uploadsServiceImpl.init();
+		
 	}
 
 }
