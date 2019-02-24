a64b15e23b35bb1d22bb469eac61ac740e7893f9
Víctor M Gomez <34237832+Victor96Man@users.noreply.github.com>
2019-02-24 10:02:32 +0100

    Creación clase y tabla Producto

--- src/main/java/com/bolsadeideas/springboot/app/models/entity/Factura.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/entity/Factura.java
@@ -109,5 +109,17 @@ public class Factura implements Serializable {
 		this.items.add(item);
 	}
 
+	public Double getTotal() {
+		
+		Double total = 0.0;
+		int size = items.size();
+		
+		for (int i = 0; i < size; i++) {
+			total += items.get(i).calcularImporte();
+		}
+		
+		return total;
+	}
+
 	private static final long serialVersionUID = 1L;
 }
