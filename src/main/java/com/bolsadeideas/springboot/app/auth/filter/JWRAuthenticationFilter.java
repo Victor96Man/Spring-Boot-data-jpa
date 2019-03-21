07d8fb36d4719e4c05934c79318ee912c4518407
Víctor M Gomez <34237832+Victor96Man@users.noreply.github.com>
2019-03-21 11:06:00 +0100

    Generando Token y autenticado por body y por raw

--- src/main/java/com/bolsadeideas/springboot/app/auth/filter/JWRAuthenticationFilter.java
+++ src/main/java/com/bolsadeideas/springboot/app/auth/filter/JWRAuthenticationFilter.java
@@ -1,5 +1,13 @@
 package com.bolsadeideas.springboot.app.auth.filter;
 
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -7,41 +15,83 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-public class JWRAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
-	
+import com.bolsadeideas.springboot.app.models.entity.Usuario;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+public class JWRAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
 	private AuthenticationManager authenticationManager;
 
-	
 	public JWRAuthenticationFilter(AuthenticationManager authenticationManager) {
 		this.authenticationManager = authenticationManager;
 		setRequiresAuthenticationRequestMatcher(new AntPathRequestMatcher("/api/login", "POST"));
 	}
 
-
 	@Override
 	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
 			throws AuthenticationException {
-		
+
 		String username = obtainUsername(request);
 		String password = obtainPassword(request);
 
-		if (username == null) {
-			username = "";
-		}
-
-		if (password == null) {
-			password = "";
+		if (username == null && password == null) {
+			Usuario user = null;
+			try {
+				user = new ObjectMapper().readValue(request.getInputStream(), Usuario.class);
+				username = user.getUsername();
+				password = user.getPassword();
+			} catch (JsonParseException e) {
+				e.printStackTrace();
+			} catch (JsonMappingException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 
 		username = username.trim();
-		
+
 		UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password);
-		
+
 		return authenticationManager.authenticate(authToken);
 	}
 
-	
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+			Authentication authResult) throws IOException, ServletException {
+
+		String username = ((User) authResult.getPrincipal()).getUsername();
+
+		Collection<? extends GrantedAuthority> roles = authResult.getAuthorities();
+
+		Claims claims = Jwts.claims();
+		claims.put("authorities", new ObjectMapper().writeValueAsString(roles));
+
+		String token = Jwts.builder().setClaims(claims).setSubject(username)
+				.signWith(SignatureAlgorithm.HS512, "Alguna.Clave.Secreta.123456".getBytes()).setIssuedAt(new Date())
+				.setExpiration(new Date(System.currentTimeMillis() + 14000000L)).compact();
+
+		response.addHeader("Authorization", "Bearer " + token);
+
+		Map<String, Object> body = new HashMap<String, Object>();
+		body.put("token", token);
+		body.put("user", (User) authResult.getPrincipal());
+		body.put("mensaje", String.format("Hola %s, has iniciado sesión con éxito!", username));
+
+		response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+		response.setStatus(200);
+		response.setContentType("application/json");
+	}
+
 }
